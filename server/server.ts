742eeeda0c89bfa4b8b5358a5f361f5d132447f1
Norman Qian <normanqian@gmail.com>
2024-06-11 23:19:47 -0400

    backend requests

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,38 @@
-import express from "express";
-import cors from "cors";
+import "dotenv/config"; // To read CLERK_SECRET_KEY and CLERK_PUBLISHABLE_KEY
+import {
+  ClerkExpressWithAuth,
+  LooseAuthProp,
+  WithAuthProp,
+} from "@clerk/clerk-sdk-node";
+import express, { Application, Request, Response } from "express";
 
-const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(cors());
+const app: Application = express();
 
-app.get("/", (req, res) => {
-  res.send("success");
+declare global {
+  namespace Express {
+    interface Request extends LooseAuthProp {}
+  }
+}
+
+// Use the lax middleware that returns an empty auth object when unauthenticated
+app.get(
+  "/protected-route",
+  ClerkExpressWithAuth({
+    // Add options here
+    // See the Middleware options section for more details
+  }),
+  (req: WithAuthProp<Request>, res: Response) => {
+    res.json(req.auth);
+  }
+);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(401).send("Unauthenticated!");
 });
 
-app.get("/secret", (req, res) => {
-  // TODO: protect this page so if the user is not logged in, they can't navigate.
-  // check isAuthed(req)
-  res.send("revealed");
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
-app.listen(8080, () => {
-  console.log(`app is listening on port 8080`);
-});
-//EOD Pull Request
